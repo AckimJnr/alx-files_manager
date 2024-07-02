010f21eb9b2ca2feffbdfcc6f6a03d43358af169
Ackim Longwe <116080565+AckimJnr@users.noreply.github.com>
2024-07-02 14:48:33 +0200

    Update server log

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const indexRoutes = require('./routes/index');
 
-const defaultPort = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use('/', indexRoutes);
 
-app.listen(process.env.PORT || defaultPort, () => {
-  console.log(`Server is running on port ${defaultPort}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
