2ee0a235cd239cc357926de1b2508e87fd699775
Ackim Longwe <116080565+AckimJnr@users.noreply.github.com>
2024-07-03 16:08:01 +0200

    Add file routes

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -86,6 +86,90 @@ class FilesController {
       return res.status(500).json({ error: `Internal Server Error: ${error}` });
     }
   }
+
+  static async getShow(req, res) {
+    const token = req.headers['x-token'];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+
+    try {
+      const userId = await redisClient.get(key);
+
+      if (!userId) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const fileId = req.params.id;
+      const FilesCollection = mongoDb.client.db().collection('files');
+      const file = await FilesCollection.findOne({
+        _id: new mongodb.ObjectId(fileId),
+        userId: new mongodb.ObjectId(userId),
+      });
+
+      if (!file) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+
+      return res.status(200).json({
+        id: file._id,
+        userId: file.userId,
+        name: file.name,
+        type: file.type,
+        isPublic: file.isPublic,
+        parentId: file.parentId,
+        localPath: file.localPath,
+      });
+    } catch (error) {
+      return res.status(500).json({ error: `Internal Server Error: ${error}` });
+    }
+  }
+
+  static async getIndex(req, res) {
+    const token = req.headers['x-token'];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+
+    try {
+      const userId = await redisClient.get(key);
+
+      if (!userId) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      const { parentId = '0', page = 0 } = req.query;
+      const FilesCollection = mongoDb.client.db().collection('files');
+
+      const query = {
+        userId: new mongodb.ObjectId(userId),
+        parentId: parentId === '0' ? '0' : new mongodb.ObjectId(parentId),
+      };
+
+      const files = await FilesCollection.find(query)
+        .skip(parseInt(page, 10) * 20)
+        .limit(20)
+        .toArray();
+
+      return res.status(200).json(files.map((file) => ({
+        id: file._id,
+        userId: file.userId,
+        name: file.name,
+        type: file.type,
+        isPublic: file.isPublic,
+        parentId: file.parentId,
+        localPath: file.localPath,
+      })));
+    } catch (error) {
+      return res.status(500).json({ error: `Internal Server Error: ${error}` });
+    }
+  }
 }
 
 module.exports = FilesController;
